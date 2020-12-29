c96622eaa96d798d77b887a7e2099d635fe6a470
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-12-28 20:51:17 -0600

    feat: separed configurations in env file

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-import pyautogui
-import webbrowser
-from time import sleep
+from typing import Counter
+from rpa import web
 
 if __name__ == "__main__":
-
-    url = "https://tinder.com/app/recs"
-    webbrowser.open_new_tab(url)
-    sleep(5)
+    web.open_url('like_url', True)
     counter = 0
-    increment = 2.5
-    reload_page_increment = 5
-    reload_page = 100
-    try:
-        while True:
-            pyautogui.moveTo(1318,798)
-            pyautogui.click()
-            print(f'Liked {counter}, {reload_page} ')
-            sleep(2.5)
-            if reload_page == counter:
-                pyautogui.moveTo(254,989)
-                pyautogui.click()
-                sleep(5)
-                reload_page = reload_page + reload_page_increment
-                print('Reload')
-            counter = counter + increment
-    except KeyboardInterrupt:
-        print('\n')
-    
-    # print('Press Ctrl-C to quit.')
-    # try:
-    #     while True:
-    #         x, y = pyautogui.position()
-    #         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-    #         print(positionStr, end='')
-    #         print('\b' * len(positionStr), end='', flush=True)
-    # except KeyboardInterrupt:
-    #     print('\n')
-
-    # term = 'Hola'
-    # url = "https://www.google.com.tr/search?q={}".format(term)
-    # webbrowser.open_new_tab(url)
+    reposition = 60
+    while True:
+        web.likeable()
+        print("Likeable")
+        web.open_url('passport_url', False)
+        web.timer()
+        web.passport()
---
+++ rpa/web.py
@@ -0,0 +1,39 @@
+import pyautogui
+from dotenv import load_dotenv
+from time import sleep
+import webbrowser
+import os
+
+load_dotenv()
+
+def timer():
+    sleep(int(os.getenv('timer')))
+
+def open_url(site, new):
+    pyautogui.hotkey('ctrlleft', 'f4')
+    webbrowser.open_new_tab(os.getenv(site))
+    timer()
+
+def passport():
+    pyautogui.moveTo(int(os.getenv('passport_x')),int(os.getenv('passport_y')))
+    pyautogui.click()
+    pyautogui.click()
+
+def likeable():
+    counter = 0.0
+    increment = float(os.getenv('increment'))
+    reload_page = float(os.getenv('reload_page'))
+    try:
+        while True:
+            pyautogui.moveTo(int(os.getenv('like_x')),int(os.getenv('like_y')))
+            pyautogui.click()
+            print(f'Liked {counter}, {reload_page} ')
+            sleep(1)
+            if reload_page == counter:
+                pyautogui.moveTo(int(os.getenv('reload_x')),int(os.getenv('reload_y')))
+                pyautogui.click()
+                timer()
+                break
+            counter = counter + increment
+    except KeyboardInterrupt:
+        print('\n')
